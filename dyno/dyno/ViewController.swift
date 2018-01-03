e116076d243589ac8656d12a4d421e1b090e400b
Laura Chen <lchen5@swarthmore.edu>
2018-01-02 19:21:12 -0800

    Added XCode files.

---
+++ dyno/dyno/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dyno
+//
+//  Created by Laura Chen on 1/2/18.
+//  Copyright © 2018 Laura Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
